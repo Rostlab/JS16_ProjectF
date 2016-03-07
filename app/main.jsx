e16148649f15bdc6005689f2278d683ad4102fb1
Yasar Kücükkaya <yasar@yk-code.de>
2016-03-07 21:59:45 +0100

    Folder structure and page updated and styleable

--- app/main.jsx
+++ app/main.jsx
@@ -4,12 +4,15 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Router, Route, browserHistory } from 'react-router';
 
-import App from './App/App.jsx';
-import About from './About/About.jsx';
+import App from './components/App/App.jsx';
+import About from './components/public/About/About.jsx';
+import Start from './components/public/Start/Start.jsx';
 
 ReactDOM.render((
   <Router history={browserHistory}>
-    <Route path="/" component={App}/>
-    <Route path="/about" component={About}/>
+    <Route path="/" component={App}>
+      <Route path="/" component={Start}/>
+      <Route path="/about" component={About}/>
+    </Route>
   </Router>
 ), document.getElementById('root'));
