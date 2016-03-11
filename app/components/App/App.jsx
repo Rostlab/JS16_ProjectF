d06ff014570310aff73f521829c52dda5f4ba128
Georgi Anastasov <georgi.anastasov@tum.de>
2016-03-12 00:25:48 +0100

    React-bootstrap added

--- app/components/App/App.jsx
+++ app/components/App/App.jsx
@@ -5,7 +5,7 @@ import Header from './Header/Header.jsx';
 import Footer from './Footer/Footer.jsx';
 
 import Start from '../public/Start/Start.jsx';
-
+// import 'bootstrap/dist/css/bootstrap.css';
 export default class App extends Component {
   render() {
     return (
--- app/main.jsx
+++ app/main.jsx
@@ -1,5 +1,5 @@
 /*eslint-env browser*/
-
+'use strict';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Router, Route, browserHistory } from 'react-router';
@@ -7,6 +7,9 @@ import { Router, Route, browserHistory } from 'react-router';
 import App from './components/App/App.jsx';
 import About from './components/public/About/About.jsx';
 import Start from './components/public/Start/Start.jsx';
+
+import 'bootstrap-webpack';
+import 'jquery';
 
 ReactDOM.render((
   <Router history={browserHistory}>
@@ -17,3 +20,4 @@ ReactDOM.render((
     <Route path="*" component={App}/>
   </Router>
 ), document.getElementById('root'));
+
