4bddeac708dfca8500494ef72368b474f786b5a9
Yasar Kücükkaya <yasar@yk-code.de>
2016-03-12 09:37:10 +0100

    Fixed build

--- webpack.production.config.js
+++ webpack.production.config.js
@@ -30,7 +30,17 @@ module.exports = {
       {
         test: /\.css$/,
         loader: ExtractTextPlugin.extract('style', 'css?modules!postcss')
-      }
+      },
+      // **IMPORTANT** This is needed so that each bootstrap js file required by
+      // bootstrap-webpack has access to the jQuery object
+      { test: /bootstrap\/js\//, loader: 'imports?jQuery=jquery' },
+      
+      // Needed for the css-loader when [bootstrap-webpack](https://github.com/bline/bootstrap-webpack)
+      // loads bootstrap's css.
+      { test: /\.(woff|woff2)$/, loader:"url?prefix=font/&limit=5000" },
+      { test: /\.ttf(\?v=\d+\.\d+\.\d+)?$/,    loader: "url?limit=10000&minetype=application/octet-stream" },
+      { test: /\.eot(\?v=\d+\.\d+\.\d+)?$/,    loader: "file" },
+      { test: /\.svg(\?v=\d+\.\d+\.\d+)?$/,    loader: "url?limit=10000&minetype=image/svg+xml" }
     ]
   },
   postcss: [
