09c6d1c819540218778c967ce4a3cf9a36dd76ef
Andrew094 <andi.s2011@yahoo.de>
2016-03-14 00:13:55 +0100

    include url loader for webpack

--- webpack.config.js
+++ webpack.config.js
@@ -50,13 +50,13 @@ module.exports = {
                 loader: "url?prefix=font/&limit=5000"
             }, {
                 test: /\.ttf(\?v=\d+\.\d+\.\d+)?$/,
-                loader: "url?limit=10000&minetype=application/octet-stream"
+                loader: "url?limit=10000&mimetype=application/octet-stream"
             }, {
                 test: /\.eot(\?v=\d+\.\d+\.\d+)?$/,
                 loader: "file"
             }, {
                 test: /\.svg(\?v=\d+\.\d+\.\d+)?$/,
-                loader: "url?limit=10000&minetype=image/svg+xml"
+                loader: "url?limit=10000&mimetype=image/svg+xml"
             }
 
         ]
