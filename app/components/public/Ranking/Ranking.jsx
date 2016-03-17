6c5f0dd1f69968cbced7463599e09bc21e7c81d3
Yasar Kücükkaya <yasar@yk-code.de>
2016-03-17 18:52:40 +0100

    Added ranking page

---
+++ app/components/public/Ranking/Ranking.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+let {Component} = React;
+
+export default class Ranking extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Ranking</h1>
+      </div>
+    );
+  }
+}
