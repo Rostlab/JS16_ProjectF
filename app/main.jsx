b97d0b44dcb212bab2767a086c4b4616bb4e2108
Yasar Kücükkaya <yasar@yk-code.de>
2016-03-17 16:01:20 +0100

    Extended routing

--- app/main.jsx
+++ app/main.jsx
@@ -2,24 +2,29 @@
 'use strict';
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Router, Route, browserHistory } from 'react-router';
+import { Router, Route, browserHistory, IndexRoute } from 'react-router';
 
 import 'jquery';
 import 'bootstrap/dist/css/bootstrap.css';
 import 'bootstrap/dist/js/bootstrap.js';
 
-import App from './components/App/App.jsx';
+import App from './components/app/App.jsx';
 import About from './components/public/About/About.jsx';
 import Start from './components/public/Start/Start.jsx';
 import Character from './components/public/Character/Character.jsx';
+import CharacterList from './components/public/CharacterList/CharacterList.jsx';
+import Site404 from './components/public/404/404.jsx';
 
 ReactDOM.render((
   <Router history={browserHistory}>
     <Route path="/" component={App}>
-      <Route path="/" component={Start}/>
-      <Route path="/about" component={About}/>
-      <Route path="/character" component={Character}/>
+      <IndexRoute component={Start}/>
+      <Route path="about" component={About}/>
+      <Route path="characters" component={CharacterList}>
+        <IndexRoute component={CharacterList} />
+        <Route path=":charID" component={Character}/>
+      </Route>
     </Route>
-    <Route path="*" component={App}/>
+    <Route path="*" component={Site404}/>
   </Router>
 ), document.getElementById('root'));
