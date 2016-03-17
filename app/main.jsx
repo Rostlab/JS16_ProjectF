8a129e0bd7c87a08ac19bc3981b01b9ef983416d
Yasar Kücükkaya <yasar@yk-code.de>
2016-03-17 16:02:10 +0100

    Renamed Characters

--- app/main.jsx
+++ app/main.jsx
@@ -11,7 +11,7 @@ import 'bootstrap/dist/js/bootstrap.js';
 import App from './components/app/App.jsx';
 import About from './components/public/About/About.jsx';
 import Start from './components/public/Start/Start.jsx';
-import Character from './components/public/Character/Character.jsx';
+import Character from './components/public/Characters/Characters.jsx';
 import CharacterList from './components/public/CharacterList/CharacterList.jsx';
 import Site404 from './components/public/404/404.jsx';
 
