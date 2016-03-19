c683817fb3b254cd84b02b3e94beed38bd7bca4a
Georgi Anastasov <georgi.anastasov@tum.de>
2016-03-19 03:37:35 +0100

    Repair header navigation

--- app/components/app/App.jsx
+++ app/components/app/App.jsx
@@ -15,7 +15,7 @@ export default class App extends Component {
     return (
       <div>
         <Header />
-        <Grid fluid>
+        <Grid fluid className="main-container">
             {this.props.children || <Start />}
         </Grid>
         <Footer />
