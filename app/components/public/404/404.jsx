e089aaa6b83c74718566171a2794bcbedafb8af8
Georgi Anastasov <georgi.anastasov@tum.de>
2016-03-19 03:02:32 +0100

    404 fix 2

--- app/components/public/404/404.jsx
+++ app/components/public/404/404.jsx
@@ -3,18 +3,18 @@ let {Component} = React;
 import './404.css';
 
 export default class Site404 extends Component {
-  render() {
-    return (
-      <div className="notfound">
-    	<div className="cloud"></div>
-    	<div className="cloud2"></div>
-    	<div className="hodor"></div>
-    	<div className="message">
-	        <h1>404</h1>
-	        <h2>Page could not be found..</h2>
-	        <h4>..neither could Hodor.</h4>
-	    </div>
-      </div>
-    );
-  }
+	render() {
+	return (
+		<div className="notfound">
+		<div className="cloud"></div>
+		<div className="cloud2"></div>
+		<div className="hodor"></div>
+		<div className="message">
+			<h1>404</h1>
+			<h2>Page could not be found..</h2>
+			<h4>..neither could Hodor.</h4>
+		</div>
+	</div>
+	);
+	}
 }
