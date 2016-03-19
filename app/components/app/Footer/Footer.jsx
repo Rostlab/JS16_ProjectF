88a34aca6b8bf6a9c5fd74c882156373fd4bd96b
Georgi Anastasov <georgi.anastasov@tum.de>
2016-03-19 02:40:49 +0100

    New good looking Hodor page(404)

--- app/components/app/Footer/Footer.jsx
+++ app/components/app/Footer/Footer.jsx
@@ -5,7 +5,7 @@ let {Component} = React;
 export default class Footer extends Component {
   render() {
     return (
-      <div>
+      <div className="footer">
         Footer
       </div>
     );
--- app/components/public/404/404.jsx
+++ app/components/public/404/404.jsx
@@ -1,11 +1,19 @@
 import React from 'react';
 let {Component} = React;
+import './404.css';
 
 export default class Site404 extends Component {
   render() {
     return (
-      <div>
-        <h1>404</h1>
+      <div className="notfound">
+    	<div className="cloud"></div>
+    	<div className="cloud2"></div>
+    	<div className="hodor"></div>
+    	<div className="message">
+	        <h1>404</h1>
+	        <h2>Page could not be found..</h2>
+	        <h4>..neither could Hodor.</h4>
+	    </div>
       </div>
     );
   }
