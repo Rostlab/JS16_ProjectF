622e545f837970de20e44f84e6998dd3d25a4b9d
Andrew094 <andi.s2011@yahoo.de>
2016-03-19 16:46:12 +0100

    restructure footer

--- app/components/app/Footer/Footer.jsx
+++ app/components/app/Footer/Footer.jsx
@@ -1,13 +1,51 @@
 import React from 'react';
 let {Component} = React;
-//import styles from './Footer.css';
+import './Footer.css';
+import Row from 'react-bootstrap/lib/Row';
+import Column from 'react-bootstrap/lib/Col';
 
 export default class Footer extends Component {
   render() {
     return (
-      <div className="footer">
-        Footer
-      </div>
+        <footer>
+          <div className="container">
+            <Row>
+              <Column md={4}>
+                <h4>About</h4>
+                <hr />
+                <p>
+                  We provide you with a lot of interesting Game of Thrones data.
+                </p>
+              </Column>
+              <Column md={4}>
+                <h4>Social</h4>
+                <hr />
+                <p>
+                  <a href="#">Facebook</a>
+                </p>
+                <p>
+                  <a href="#">Twitter</a>
+                </p>
+                <p>
+                  <a href="#">Google Plus</a>
+                </p>
+              </Column>
+              <Column md={4}>
+                <h4>Legal</h4>
+                <hr />
+                <p>
+                  <a href="#">Imprint</a>
+                </p>
+                <p>
+                  <a href="#">Privacy</a>
+                </p>
+                <p>
+                  <a href="#">TUM</a>
+                </p>
+              </Column>
+            </Row>
+          </div>
+        </footer>
     );
   }
 }
---
+++ app/components/app/Footer/FooterCopy.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+let {Component} = React;
+import './Footer.css';
+import Row from 'react-bootstrap/lib/Row';
+import Column from 'react-bootstrap/lib/Col';
+
+export default class FooterCopy extends Component {
+    render() {
+        return (
+            <div className="footer-copy">
+
+                <div className="container">
+                    <Row>
+                        <Column md={12}>
+                            <p className="text-center">
+                                &copy; 2016 Game of Thrones All rights reserved.
+                            </p>
+                        </Column>
+                    </Row>
+                </div>
+
+            </div>
+        );
+    }
+}
