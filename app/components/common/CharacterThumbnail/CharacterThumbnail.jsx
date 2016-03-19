a162041a900cf9c857283f622afa9fb25caea4e7
Georgi Anastasov <georgi.anastasov@tum.de>
2016-03-19 23:32:06 +0100

    fixed url and thumbnail height

--- app/components/common/CharacterThumbnail/CharacterThumbnail.jsx
+++ app/components/common/CharacterThumbnail/CharacterThumbnail.jsx
@@ -1,13 +1,18 @@
 import React from 'react';
 let {Component} = React;
 import Col from 'react-bootstrap/lib/Col';
-import Thumbnail from 'react-bootstrap/lib/Thumbnail';
+import {Thumbnail} from 'react-bootstrap';
+import "./CharacterThumbnail.css";
 
 export default class CharacterThumbnail extends Component {
+	
     render() {
+		var x = (this.props.imageUrl === 
+		"https://placeholdit.imgix.net/~text?txtsize=28&txt=Fallback-Image&w=300&h=350") ? this.props.imageUrl : "https://got-api.bruck.me/"+this.props.imageUrl ;
+	
         return (
             <Col md={3}>
-                <Thumbnail src={this.props.imageUrl} >
+                <Thumbnail src={x} className="character-thumbnail" >
                     <p> {this.props.name}</p>
                 </Thumbnail>
             </Col>
