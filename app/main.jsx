310f3d3783f2d97ec4ed2c7319b4c5bf5bd8b98c
Yasar Kücükkaya <yasar@yk-code.de>
2016-03-19 17:25:03 +0100

    Removed old snippet, added ga-react-router package

--- app/main.jsx
+++ app/main.jsx
@@ -1,14 +1,15 @@
 /*eslint-env browser*/
 'use strict';
+
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Router, Route, browserHistory, IndexRoute } from 'react-router';
 
-import './analytics.js'
+import { Router, Route, IndexRoute } from 'react-router';
 
 import 'jquery';
 import 'bootstrap/dist/css/bootstrap.css';
 import 'bootstrap/dist/js/bootstrap.js';
+
 
 import App from './components/app/App.jsx';
 import About from './components/public/About/About.jsx';
@@ -19,8 +20,18 @@ import Characters from './components/public/Characters/Characters.jsx';
 import CharacterList from './components/public/CharacterList/CharacterList.jsx';
 import Site404 from './components/public/404/404.jsx';
 
-ReactDOM.render((
-  <Router history={browserHistory}>
+import ga from 'ga-react-router';
+import createBrowserHistory from 'history/lib/createBrowserHistory';
+const history = createBrowserHistory();
+
+// Listen for changes to the current location. The
+// listener is called once immediately.
+const unlisten = history.listen(location => {
+  ga('send', location);
+});
+
+ReactDOM.render(
+  <Router history={history}>
     <Route path="/" component={App}>
       <IndexRoute component={Start}/>
       <Route path="/map" component={Map}/>
@@ -33,4 +44,6 @@ ReactDOM.render((
       <Route path="*" component={Site404}/>
     </Route>
   </Router>
-), document.getElementById('root'));
+, document.getElementById('root'));
+
+unlisten();
