cf6d8c8c1b57dcf01af59292dedca582df9dc904
Mina Kleid <minazakiz@gmail.com>
2016-03-19 09:40:13 +0100

    adding plod characters

--- app/components/public/Start/Start.jsx
+++ app/components/public/Start/Start.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 let {Component} = React;
 import styles from './Start.css';
+import Stats from '../../common/Stats/Stats.jsx';
 
 export default class Start extends Component {
   render() {
     return (
       <div className={styles.wrapper}>
         Start
+        <Stats/>
       </div>
     );
   }
