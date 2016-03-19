0adc9364768df4cdd83a810a878de0def8578bd7
mammuth <kontakt@maxi-muth.de>
2016-03-19 20:34:23 +0100

    Image-Grid of the characters for the catalogue. [WIP]
    Component is based on Mina's work on that component.

---
+++ app/components/common/CharacterThumbnail/CharacterThumbnail.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+let {Component} = React;
+import Col from 'react-bootstrap/lib/Col';
+import Thumbnail from 'react-bootstrap/lib/Thumbnail';
+
+export default class CharacterThumbnail extends Component {
+    render() {
+        return (
+            <Col md={3}>
+                <Thumbnail src={this.props.imageUrl} >
+                    <p> {this.props.name}</p>
+                </Thumbnail>
+            </Col>
+        );
+    }
+}
+
+CharacterThumbnail.propTypes = { imageUrl: React.PropTypes.element, name: React.PropTypes.element.isRequired };
+CharacterThumbnail.defaultProps = { imageUrl: 'https://placeholdit.imgix.net/~text?txtsize=28&txt=Fallback-Image&w=300&h=350' };
--- app/components/public/CharacterList/CharacterList.jsx
+++ app/components/public/CharacterList/CharacterList.jsx
@@ -4,8 +4,13 @@
 
 import React from 'react';
 let {Component} = React;
+
+import { Row, Col } from 'react-bootstrap';
+
 import Store from '../../../stores/CharactersStore';
 import Actions from '../../../actions/CharactersActions';
+import CharacterThumbnail from '../../common/CharacterThumbnail/CharacterThumbnail.jsx';
+
 
 export default class CharacterList extends Component {
 
@@ -37,13 +42,17 @@ export default class CharacterList extends Component {
   render() {
     return (
       <div>
-        <h1>CharacterList {this.state.characters.length}</h1>
-        <div>{
-          this.state.characters.map(function (character) {
-            return <div key={character._id}>{character.name}</div>;
-          })
-        }
-        </div>
+        <h1>CharacterList ({this.state.characters.length})</h1>
+        <Row>
+          <Col md={8} mdOffset={2}>
+            <div>{
+              this.state.characters.map(function (character) {
+                return <CharacterThumbnail key={character._id} name={character.name} imageUrl={character.imageLink}/>;
+              })
+            }
+            </div>
+          </Col>
+        </Row>
       </div>
     );
   }
