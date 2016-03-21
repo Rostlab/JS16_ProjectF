afbf86b54d189df004ef0eb53d2d77c861c0e11d
mammuth <kontakt@maxi-muth.de>
2016-03-21 04:36:25 +0100

    Added PLOD to home-page

--- app/components/public/Start/Start.jsx
+++ app/components/public/Start/Start.jsx
@@ -3,7 +3,6 @@ let {Component} = React;
 import { Row } from 'react-bootstrap';
 
 import Stats from '../../common/Stats/Stats.jsx';
-import MapComp from '../../common/MapComp/MapComp.jsx';
 import './Start.css';
 
 export default class Start extends Component {
@@ -13,11 +12,7 @@ export default class Start extends Component {
         <Row fluid>
           <div className="header-image"></div>
         </Row>
-        Start
         <Stats/>
-        <div className="container">
-          <MapComp />
-        </div>
       </div>
     );
   }
