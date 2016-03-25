18cab8c517c6d851da729c6bc9b540a0777c41c3
Yasar Kücükkaya <yasar@yk-code.de>
2016-03-25 18:16:56 +0100

    Added d4 and d5, but it requires mongodb

--- server/server.js
+++ server/server.js
@@ -1,10 +1,10 @@
-'use strict'
+'use strict';
 
 var express = require('express');
 var path = require('path');
 var compression = require('compression');
-//var d5 = require('./d5.js');
-//var d4 = require('./d4.js');
+var d5 = require('./d5.js');
+var d4 = require('./d4.js');
 
 var app = express();
 
@@ -12,14 +12,14 @@ app.use(compression());
 
 app.use(express.static(path.join(__dirname, '../', 'build')));
 
-//app.use('/d5', d5);
-//app.use('/d4', d4);
+app.use('/d5', d5);
+app.use('/d4', d4);
 
 app.get('*', function (req, res) {
   res.sendFile(path.join(__dirname, '../', 'build', 'index.html'));
-})
+});
 
 var PORT = process.env.PORT || 8080;
 app.listen(PORT, function() {
   console.log('Production Express server running at localhost:' + PORT);
-})
+});
