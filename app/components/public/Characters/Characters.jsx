053424846d0ad05a318d0e6f454a1a16354cd59e
Yasar Kücükkaya <yasar@yk-code.de>
2016-03-29 20:24:08 +0200

    Fix searching with braces ()

--- app/components/public/Characters/Characters.jsx
+++ app/components/public/Characters/Characters.jsx
@@ -22,10 +22,7 @@ export default class Character extends Component {
     }
 
     componentDidMount() {
-        var char = decodeURIComponent(this.props.params.id);
-        char = char.replace('(', '%5C(');
-        char = char.replace(')', '%5C)');
-        Actions.loadCharacter(char);
+        Actions.loadCharacter(decodeURIComponent(this.props.params.id));
     }
 
     componentWillUnmount(){
