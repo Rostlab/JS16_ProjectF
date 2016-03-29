8cb37183938ab4ac86d2c7dee6ea912548a7b406
Mina Kleid <minazakiz@gmail.com>
2016-03-29 12:24:36 +0200

    finalizing up the twitter

--- app/actions/TwitterSentimentsActions.js
+++ app/actions/TwitterSentimentsActions.js
@@ -4,12 +4,10 @@ var Api = require('../network/SentimentAPI');
 
 var TwitterSentimentsActions = {
 
-    loadTopSentiments: function(count,startDate,endDate) {
+    loadTopSentiments: function(count) {
         Api
-            .get('d5/sentiment/top')
+            .get('d4/sentiment/top')
             .query({number: count})
-            .query({startDate: startDate})
-            .query({endDate: endDate})
             .then(function (sentiments) {
                 AppDispatcher.handleServerAction({
                     actionType: Constants.RECEIVE_TWITTER_TOP_CHARACTERS,
@@ -17,12 +15,10 @@ var TwitterSentimentsActions = {
                 });
             });
     },
-    loadFlopSentiments: function(count,startDate,endDate) {
+    loadFlopSentiments: function(count) {
         Api
-            .get('d5/sentiment/worst')
+            .get('d4/sentiment/worst')
             .query({number: count})
-            .query({startDate: startDate})
-            .query({endDate: endDate})
             .then(function (sentiments) {
                 AppDispatcher.handleServerAction({
                     actionType: Constants.RECEIVE_TWITTER_FLOP_CHARACTERS,
@@ -30,7 +26,7 @@ var TwitterSentimentsActions = {
                 });
             });
     },
-    loadTopControversial: function(count,startDate,endDate) {
+    loadControversialSentiments: function(count,startDate,endDate) {
         Api
             .get('d5/sentiment/controversial')
             .query({number: count})
@@ -43,12 +39,10 @@ var TwitterSentimentsActions = {
                 });
             });
     },
-    loadMostTalkedAbout: function(count,startDate,endDate) {
+    loadMostDiscussedSentiments: function(count) {
         Api
-            .get('d5/sentiment/talked')
+            .get('d4/sentiment/discussed')
             .query({number: count})
-            .query({startDate: startDate})
-            .query({endDate: endDate})
             .then(function (sentiments) {
                 AppDispatcher.handleServerAction({
                     actionType: Constants.RECEIVE_TWITTER_TALKED_ABOUT_CHARACTERS,
@@ -56,37 +50,10 @@ var TwitterSentimentsActions = {
                 });
             });
     },
-    loadChatacterSentiment: function(name,date) {
+    loadCharacterSentiment: function(characterId) {
         Api
-            .get('d5/sentiment')
-            .query({name: name})
-            .query({date: date})
-            .then(function (sentiments) {
-                AppDispatcher.handleServerAction({
-                    actionType: Constants.RECEIVE_TWITTER_CHARACTER_SENTIMENT,
-                    data: sentiments
-                });
-            });
-    },
-    loadCharacterSentimentByTimeframe: function(name,startDate,endDate) {
-        Api
-            .get('d5/sentiment/')
-            .query({name: name})
-            .query({startDate: startDate})
-            .query({endDate: endDate})
-            .then(function (sentiments) {
-                AppDispatcher.handleServerAction({
-                    actionType: Constants.RECEIVE_TWITTER_CHARACTER_SENTIMENT,
-                    data: sentiments
-                });
-            });
-    },
-    loadCharacterSentimentByEpisode: function(name,season,episode) {
-        Api
-            .get('d5/sentiment')
-            .query({name: name})
-            .query({season: season})
-            .query({episode: episode})
+            .get('d4/character')
+            .query({id: characterId})
             .then(function (sentiments) {
                 AppDispatcher.handleServerAction({
                     actionType: Constants.RECEIVE_TWITTER_CHARACTER_SENTIMENT,
@@ -94,6 +61,33 @@ var TwitterSentimentsActions = {
                 });
             });
     }
+    // ,
+    // loadCharacterSentimentByTimeframe: function(name,startDate,endDate) {
+    //     Api
+    //         .get('d5/sentiment/')
+    //         .query({name: name})
+    //         .query({startDate: startDate})
+    //         .query({endDate: endDate})
+    //         .then(function (sentiments) {
+    //             AppDispatcher.handleServerAction({
+    //                 actionType: Constants.RECEIVE_TWITTER_CHARACTER_SENTIMENT,
+    //                 data: sentiments
+    //             });
+    //         });
+    // },
+    // loadCharacterSentimentByEpisode: function(name,season,episode) {
+    //     Api
+    //         .get('d5/sentiment')
+    //         .query({name: name})
+    //         .query({season: season})
+    //         .query({episode: episode})
+    //         .then(function (sentiments) {
+    //             AppDispatcher.handleServerAction({
+    //                 actionType: Constants.RECEIVE_TWITTER_CHARACTER_SENTIMENT,
+    //                 data: sentiments
+    //             });
+    //         });
+    // }
 
 
 };
