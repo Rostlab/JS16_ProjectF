491346886e9f1f5e0bd5d92a2d7d42cf20bfbfcd
Yasar Kücükkaya <yasar@yk-code.de>
2016-04-01 23:30:40 +0200

    Included images folder and the json and removed some not presentable pairs

--- app/components/common/AntagonistsComp/AntagonistsComp.jsx
+++ app/components/common/AntagonistsComp/AntagonistsComp.jsx
@@ -1,15 +1,23 @@
 import React from 'react';
+//import path from 'path';
 let {Component} = React;
-import Test from './img';
+function requireAll(requireContext) {
+  return requireContext.keys().map(requireContext);
+}
+let images = requireAll(require.context("./img", false, /^\.\/.*\.jpg$/));
+
+import json from 'json!./antagonists.json';
 
 export default class AntagonistsComp extends Component {
   render() {
-    
-    console.log(Test); /*eslint no-console:0,no-undef:0*/
+    console.log(images[0], '.jpg'); /*eslint no-console:0,no-undef:0*/
 
   return (
     <div className="home">
-      asd
+      {json.map(function(pair) {
+        console.log (pair);
+        return <img src={'/assets/static/images/'+pair[0].replace(' ','_')+'.jpg'}/>;
+      })}
     </div>
     );
   }
