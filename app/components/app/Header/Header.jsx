66d7802df920a01e68e1ef2847385e2a82f40f2a
Yasar Kücükkaya <yasar@yk-code.de>
2016-04-02 23:25:32 +0200

    Removed dropdown, changed 'Character'-Tab to 'Characters'-Tab and removed 'Home'-Tab

--- app/components/app/Header/Header.jsx
+++ app/components/app/Header/Header.jsx
@@ -2,19 +2,12 @@ import React from 'react';
 
 import { Navbar, Nav, Button, NavDropdown } from 'react-bootstrap';
 import { IndexLinkContainer, LinkContainer} from 'react-router-bootstrap';
-//import { browserHistory } from 'react-router';
 import "./Header.css";
 
 let {Component} = React;
 
 export default class Header extends Component {
     render() {
-        let dropdownactive = "";
-        if (["/about","/contribute","/credits"].indexOf(window.location.pathname) != -1) { /*eslint no-undef:0*/
-            dropdownactive = "active";
-        } else {
-            dropdownactive = "";
-        }
         return (
             <header>
                 <Navbar inverse>
@@ -28,11 +21,8 @@ export default class Header extends Component {
                     </Navbar.Header>
                     <Navbar.Collapse>
                         <Nav>
-                            <IndexLinkContainer to={{ pathname: '/'}}>
-                                <Button bsStyle="link">Home</Button>
-                            </IndexLinkContainer>
                             <LinkContainer to={{ pathname: '/characters'}}>
-                                <Button bsStyle="link">Character</Button>
+                                <Button bsStyle="link">Characters</Button>
                             </LinkContainer>
                             <LinkContainer to={{ pathname: '/statistics'}}>
                                 <Button bsStyle="link">Statistics</Button>
@@ -43,22 +33,13 @@ export default class Header extends Component {
                             <LinkContainer to={{ pathname: '/map'}}>
                                 <Button bsStyle="link">Map</Button>
                             </LinkContainer>
-                            <NavDropdown className={dropdownactive} title="About" id="basic-nav-dropdown">
-                                <LinkContainer to={{ pathname: '/about'}}>
-                                    <Button bsStyle="link">About us</Button>
-                                </LinkContainer>
-                                <LinkContainer to={{ pathname: '/credits'}}>
-                                    <Button bsStyle="link">Credits</Button>
-                                </LinkContainer>
-                                <LinkContainer to={{ pathname: '/contribute'}}>
-                                    <Button bsStyle="link">Contribute</Button>
-                                </LinkContainer>
-                            </NavDropdown>
+                            <LinkContainer to={{ pathname: '/about'}}>
+                                <Button bsStyle="link">About</Button>
+                            </LinkContainer>
                         </Nav>
                     </Navbar.Collapse>
                 </Navbar>
             </header>
         );
-
     }
 }
