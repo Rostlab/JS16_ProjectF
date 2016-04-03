530cf082701ad03f266c6457454455aabf3bdf3e
Georgi Anastasov <georgi.anastasov@tum.de>
2016-04-03 05:13:57 +0200

    Go back button from the black hole 404

--- app/components/public/404/404.jsx
+++ app/components/public/404/404.jsx
@@ -2,6 +2,8 @@ import React from 'react';
 let {Component} = React;
 import './404.css';
 
+import { LinkContainer} from 'react-router-bootstrap';
+import { Button } from 'react-bootstrap';
 export default class Site404 extends Component {
 	render() {
 	return (
@@ -13,6 +15,9 @@ export default class Site404 extends Component {
 			<h1>404</h1>
 			<h2>Page could not be found..</h2>
 			<h4>..neither could Hodor.</h4>
+			<LinkContainer to={{ pathname: '/'}}>
+                <Button>Go back to safety</Button>
+            </LinkContainer>
 		</div>
 	</div>
 	);
