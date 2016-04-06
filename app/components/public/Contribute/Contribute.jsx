14780c8adf270805d412869e610296408cc028dd
Andrew094 <andi.s2011@yahoo.de>
2016-04-06 14:57:25 +0200

    add contribute.md

--- app/components/public/Contribute/Contribute.jsx
+++ app/components/public/Contribute/Contribute.jsx
@@ -3,15 +3,14 @@ let {Component} = React;
 import Row from 'react-bootstrap/lib/Row';
 import Column from 'react-bootstrap/lib/Col';
 
+import ContributeText from '../../../../static/contribute.md';
+
 export default class Contribute extends Component {
   render() {
     return (
         <Row>
           <Column md={8} mdPush={2}>
-            <h1>For Developers</h1>
-            <p>We crawled a lot of data from primarly the <a href="awoiaf.westeros.org/index.php" target="_blank">AWOIAF wiki</a> but also other sources and we'd love to share our database with you guys!
-              <br />Check out our <a href="https://api.got.show/doc/" target="_blank">API Reference here.</a>
-            </p>
+            <ContributeText />
           </Column>
         </Row>
     );
