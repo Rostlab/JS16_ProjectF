497d2917e6279a5ff50cd72b5e3da031c44774a9
Yasar Kücükkaya <yasar@yk-code.de>
2016-04-09 22:55:57 +0200

    Added sort by plod

---
+++ app/components/common/CharacterList/CharacterList.jsx
@@ -0,0 +1,38 @@
+
+import React, {Component} from 'react';
+import CharacterThumbnail from '../CharacterThumbnail/CharacterThumbnail.jsx';
+
+import { Row, Col} from 'react-bootstrap';
+
+export default class CharacterList extends Component {
+  constructor (props) {
+    super(props);
+  }
+  render() {
+    var found = "";
+    if(this.props.loaded == false) {
+      found = "Loading ...";
+    } else if (this.props.data.length === 0) {
+      found = "Nothing found, please search something else";
+    }
+    return (
+      <div>
+        <Row>
+          <Col md={8} mdOffset={2}>
+            <div> {
+              this.props.data.map(function (character) {
+                return <CharacterThumbnail key={character._id} name={character.name} imageUrl={character.imageLink}/>;
+              })
+            }
+            </div>
+          </Col>
+          <div className="center">
+            <h3>{ found }</h3>
+          </div>
+        </Row>
+      </div>
+    );
+  }
+}
+CharacterList.propTypes = { data: React.PropTypes.array.isRequired};
+CharacterList.propTypes = { loaded: React.PropTypes.bool.isRequired};
