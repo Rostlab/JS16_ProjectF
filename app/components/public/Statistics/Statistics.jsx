06dd2c58ff1d1e71db751e3c7f0b7396a1cb0b79
Guy Yachdav <gyachdav@rostlab.org>
2016-04-13 14:50:50 -0400

    Fixed build errors

--- app/components/public/Statistics/Statistics.jsx
+++ app/components/public/Statistics/Statistics.jsx
@@ -2,6 +2,8 @@ import React from 'react';
 let {Component} = React;
 import { Grid, Row, Col } from 'react-bootstrap';
 import $ from 'jquery';
+import './Statistics.css';
+
 let js = require('raw!./src.js'); /*eslint no-undef:0 */
 
 export default class Statistics extends Component {
@@ -20,8 +22,8 @@ export default class Statistics extends Component {
           <h2>Plenty of characters to go around</h2>
           <span>Our first step was to go and collect all data we can find about the characters mentioned in the five Ice and Fire books, novellas and reader&#39;
             s companions. Turns out there&rsquo;s quite a few of them, 2028 characters to be exact. And they all have a part to play in moving the plot forward. You can see in Figure 1 how many characters (by gender) mentioned in each of the books. &nbsp;</span>
-            <div class="center" id="new_characters_introduced"></div>
-            <p><span class="caption">Figure 1. Population by gender in the Ice and Fire books</span>
+            <div className="center" id="new_characters_introduced"></div>
+            <p><span className="caption">Figure 1. Population by gender in the Ice and Fire books</span>
             </p>
             <p><span></span>
             </p>
@@ -37,10 +39,10 @@ export default class Statistics extends Component {
             </p>
             <p><span></span>
             </p>
-            <div class="center" id="characters_per_episode"></div>
+            <div className="center" id="characters_per_episode"></div>
             <p><span></span>
             </p>
-            <p><span class="caption">Figure 2. Number of characters  and new characters presented in each of the Game of Thrones episodes.
+            <p><span className="caption">Figure 2. Number of characters  and new characters presented in each of the Game of Thrones episodes.
 </span>
               <p><span></span>
               </p>
@@ -69,8 +71,8 @@ export default class Statistics extends Component {
               </p>
               <p><span></span>
               </p>
-              <div class="center" id="dead_and_alive"></div>
-              <p><span class="caption">Figure 3. Distribution of men and women within noble/non-noble and alive/dead characters.</span>
+              <div className="center" id="dead_and_alive"></div>
+              <p><span className="caption">Figure 3. Distribution of men and women within noble/non-noble and alive/dead characters.</span>
               </p>
               <h1 id="h.m7qy93gzi4my"><span>Now let&rsquo;
                 s look at the question at hand - who is likelier to die next?</span></h1>
@@ -82,9 +84,9 @@ export default class Statistics extends Component {
               </p>
               <p><span></span>
               </p>
-              <div class="center" id="distribution_plods"></div>
+              <div className="center" id="distribution_plods"></div>
               <p>
-                <span class="caption">Figure 4. Men are likelier to die than women. For instance, 21% of all men (Y-axis) are at </span><span>60% </span><span>likelihood of death (X-axis)</span><span>, which is depicted by the </span><span>blue star. For women, this ratio is only at 9% at the same 60% likelihood of death, which is depicted by the red star.</span>
+                <span className="caption">Figure 4. Men are likelier to die than women. For instance, 21% of all men (Y-axis) are at </span><span>60% </span><span>likelihood of death (X-axis)</span><span>, which is depicted by the </span><span>blue star. For women, this ratio is only at 9% at the same 60% likelihood of death, which is depicted by the red star.</span>
               </p>
               <p><span></span>
               </p>
@@ -93,8 +95,8 @@ export default class Statistics extends Component {
               </p>
               <p><span></span>
               </p>
-              <div class="center" id="distribution_nobles_plods"></div>
-              <p><span class="caption">Figure 5. Nobles are at the same risk to die as peasants. Similar to Figure 4, plotted are percentages of noble (blue line) and peasant (red line) characters (Y-axis) for whom we predict a certain likelihood of death (X-axis).For nobles, the average PLOD is 31% and for peasants it is 27%.</span>
+              <div className="center" id="distribution_nobles_plods"></div>
+              <p><span className="caption">Figure 5. Nobles are at the same risk to die as peasants. Similar to Figure 4, plotted are percentages of noble (blue line) and peasant (red line) characters (Y-axis) for whom we predict a certain likelihood of death (X-axis).For nobles, the average PLOD is 31% and for peasants it is 27%.</span>
               </p>
               <p><span></span>
               </p>
@@ -103,8 +105,8 @@ export default class Statistics extends Component {
               <p><span>Now what about a character&rsquo;s age? Which age groups are likelier to be at greater risk of death than others? Check out Figure 6, which shows a histogram of age groups and their average predicted likelihoods of death. Unsurprisingly, the period when a character rises to prominence (between the age 21-40) is also the most precarious time in a character&rsquo;
                 s life. The good news for characters who manage to live into their 70s (an unusually old age in the Ice and Fire world) is that they are likelier to die of old age. &nbsp; </span>
               </p>
-              <div class="center" id="distribution_plods_age_distribution"></div>
-              <p><span class="caption">Figure 6. Characters of age groups 21-40 and 51-60 (X-axis) are at highest risk of death (Y-axis).</span>
+              <div className="center" id="distribution_plods_age_distribution"></div>
+              <p><span className="caption">Figure 6. Characters of age groups 21-40 and 51-60 (X-axis) are at highest risk of death (Y-axis).</span>
               </p>
               <p><span></span>
               </p>
@@ -122,8 +124,8 @@ export default class Statistics extends Component {
                 which episode in the show could be the most suspenseful</span><span>&nbsp;- by say cramming many characters with high risk of dying into an episode and putting viewers at the edge of their sears. </span><span>We did just that </span><span>- we took our predicted risk of death values and averaged them for all characters appearing in each episode. </span><span>The result can be seen in Figure 7. </span><span>I don&#39;
                 t know about you, but it seems like</span><span>&nbsp;Seasons 1 and Season 2 appear to be clear winners in terms of the amount of potential suspense packed into them</span><span>. What do you think? Were the first two seasons most suspenseful? </span><span><a
                 href="http://goo.gl/YIEa7w">Vote here</a></span><span>.</span>
-              <div class="center" id="avgPLOD_per_episode"></div>
-              <p><span class="caption">Figure 7. Predicted likelihood of death averaged across characters in each episode that was aired in the first five seasons of Game of Thrones.</span>
+              <div className="center" id="avgPLOD_per_episode"></div>
+              <p><span className="caption">Figure 7. Predicted likelihood of death averaged across characters in each episode that was aired in the first five seasons of Game of Thrones.</span>
               </p>
             </p>
         </Col>
