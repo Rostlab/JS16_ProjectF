0754a994e3a4f03e08c68bef6bb8d13e87e2d4bd
Christian Dallago <sacdallago@users.noreply.github.com>
2016-04-16 02:03:36 +0200

    Revert "Fixed Tysha issue"

--- app/components/public/Characters/Characters.jsx
+++ app/components/public/Characters/Characters.jsx
@@ -47,13 +47,15 @@ export default class Character extends Component {
 
     _onChange() {
         const character = Store.getCharacter();
+        this.setState({
+            character: character,
+            sentiment: SentimentStore.getCharacterSentiment()
+        });
 
         const check = !character.dateOfDeath && character.gotplod && character.gotarffplod;
         this.setState({
             plod: (check) ? parseInt(character.gotplod.plod) || 0 : 100,
-            plodText: (check) ? '%(percent)s%' : 'D E A D',
-            character: character,
-            sentiment: SentimentStore.getCharacterSentiment() | { positive: 0, negative: 0}
+            plodText: (check) ? '%(percent)s%' : 'D E A D'
         });
     }
     render() {
