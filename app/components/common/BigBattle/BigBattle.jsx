8d5a1c2a53bdf263ec5fc43c9ac50eb1c72f8cd7
Yasar Kücükkaya <yasar@yk-code.de>
2016-04-22 01:48:19 +0200

    Revert "Revert "added copyright to startpage""
    This reverts commit d9f93c67ba5c51da42adf915fc92c92d812e3cc5.

--- app/components/common/BigBattle/BigBattle.jsx
+++ app/components/common/BigBattle/BigBattle.jsx
@@ -39,6 +39,7 @@ export default class BigBattle extends Component {
               </Link>
               <img className="antagonistTombstone" src={tombstone} />
               <p className="antagonistTombstonePlod">{parseInt(this.props.plod1)}%</p>
+              <p className="copyRight">© 2016 Home Box Office, Inc. / Sky All rights reserved.</p>
             </div>
           </Col>
           <Col xs={6}>
@@ -48,6 +49,7 @@ export default class BigBattle extends Component {
               </Link>
               <img className="antagonistTombstone" src={tombstone} />
               <p className="antagonistTombstonePlod">{parseInt(this.props.plod2)}%</p>
+              <p className="copyRight">© 2016 Home Box Office, Inc. / Sky All rights reserved.</p>
             </div>
           </Col>
         </Row>
