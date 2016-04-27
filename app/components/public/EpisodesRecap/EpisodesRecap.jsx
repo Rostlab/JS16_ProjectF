78f7e2513a29a5e5ddcf741300be2519d2d58c9c
Yasar Kücükkaya <yasar@yk-code.de>
2016-04-27 18:34:13 +0200

    redesign disclaimer

--- app/components/public/EpisodesRecap/EpisodesRecap.jsx
+++ app/components/public/EpisodesRecap/EpisodesRecap.jsx
@@ -11,19 +11,19 @@ export default class Credits extends Component {
         <Row>
           <Col md={8} mdOffset={2}>
             <h1>Season Six Episode One - The Red Woman - A got.show Recap.</h1>
-            <p>Disclaimer: this blogpost is dark and full of spoilers! If you hate spoilers, just don’t read any further! In case you decide to stay, don't worry - you’re safe. Nobody is going to kill your session.</p>
+            <div className="disclaimer"><strong>Disclaimer</strong>: this blogpost is dark and full of <strong>spoilers</strong>! If you hate spoilers, just don’t read any further! In case you decide to stay, don't worry - you’re safe. Nobody is going to kill your session.</div>
             <p>Season six is finally here and so our watch begins. It seems like Westeros is still the same batshit crazy, stinky, fanatic, murderous, and exciting show in town! Stay tuned for news from Castle Black, but first let's hop over to Dorne
               where clueless <a
-                href="https://www.got.show/characters/Doran%2520Martell">
+                href="/characters/Doran%2520Martell">
                   Doran Martell
               </a> has been stabbed multiple times by his brother's-lover <a
-              href="https://www.got.show/characters/Ellaria%2520Sand">Ellaria
+              href="/characters/Ellaria%2520Sand">Ellaria
               Sand</a> Well, we definitely knew that Doran is unlikely to survive for long - his
             Predicted Likelihood of Death (PLoD) was at 82%, which means he should&rsquo;ve
               watched his back (or front) instead of gazing into the nothing..</p>
             <p><span>&hellip; And of course a full-on coup cannot be complete without killing off the heir apparent. </span><span
             ><a
-              href="https://www.got.show/characters/Trystane%2520Martell">Trystane
+              href="/characters/Trystane%2520Martell">Trystane
               Martell</a></span><span>&nbsp;was about to embark on a cruise when all of a sudden two of the Sand Snakes confronted him with the existential question &ldquo;which one of us should kill you?&rdquo;
               Had </span><span>Trystane </span><span>checked our prediction ahead of time he would have known that he didn&rsquo;t stand a chance, his </span><span
             >PLoD was at 90%</span><span>&nbsp;anyway. With the departure of Doran and Trystane the Martell&rsquo;s are about to lose their grip over Dorne. &nbsp;</span>
@@ -32,16 +32,16 @@ export default class Credits extends Component {
             <p><span>Meanwhile, Jamie comes back from Dorne with yet another dead Lannister child. In her grief, Cersei reveals to Jaime that she knew all along that none of her kids will survive after she visited the got.show website. Since Cersey is living in a pseudo-medieval world she replaced &ldquo;machine learning algorithm&rdquo;
               with &ldquo;witch&rdquo; and &ldquo;prediction&rdquo; with &ldquo;prophecy&rdquo; but we all know what she really meant. Don&#39;t we? At any rate, </span><span>Now that the other Baratheon kids are gone, it doesn&#39;t look well for </span><span
             ><a
-              href="https://www.got.show/characters/Tommen%2520Baratheon">Tommen</a></span><span
+              href="/characters/Tommen%2520Baratheon">Tommen</a></span><span
             >&nbsp;having a PLoD at 97%</span><span>. </span></p>
             <p><span></span></p>
             <p><span>Oh and what about </span><span><a
-              href="https://www.got.show/characters/Jon%2520Snow">Jon</a></span><span>? Well, only ten short months after little </span><span>Olly</span><span>&nbsp;stabbed the Lord Commander </span><span>(</span><span>screw you Olly by the way)</span><span>, </span><span
+              href="/characters/Jon%2520Snow">Jon</a></span><span>? Well, only ten short months after little </span><span>Olly</span><span>&nbsp;stabbed the Lord Commander </span><span>(</span><span>screw you Olly by the way)</span><span>, </span><span
             ><a
-              href="https://www.got.show/characters/Davos%2520Seaworth">Davos
+              href="/characters/Davos%2520Seaworth">Davos
               Seaworth</a></span><span>&nbsp;finally opened his chamber door</span><span>&nbsp;to find Jon lying in the courtyard all by himself. And although his body was ice cold and </span><span
             ><a
-              href="https://www.got.show/characters/Eddison%2520Tollett">Edd</a></span><span>&nbsp;reached over and closed Jon&rsquo;s eye lids - don&rsquo;t think for a moment that Jon&rsquo;s storyline is over! As the sea bear put it: &ldquo;There&rsquo;s always the red woman.&rdquo;
+              href="/characters/Eddison%2520Tollett">Edd</a></span><span>&nbsp;reached over and closed Jon&rsquo;s eye lids - don&rsquo;t think for a moment that Jon&rsquo;s storyline is over! As the sea bear put it: &ldquo;There&rsquo;s always the red woman.&rdquo;
               Or whatever that thing is</span><span>.</span></p>
             <p><span></span></p>
             <p><img className="img-full-width" src="images/image00.jpg"/></p>
@@ -50,7 +50,7 @@ export default class Credits extends Component {
             <p></p>
             <p>P.S.</p>
             <p>We gotta admit our first miss as well - <a
-            href="https://www.got.show/characters/Areo%2520Hotah">Areo
+            href="/characters/Areo%2520Hotah">Areo
                  Hotah</a>, Doran&rsquo;s faithful body guard was taken out. Poor Areo didn&#39;t know what was coming, he was probably happy to see that his
             PLoD was at 36% and so he felt kinda safe. Well, Areo, you know what they say about being complacent...
             </p>
