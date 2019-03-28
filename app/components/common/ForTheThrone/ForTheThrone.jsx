5516599d4873cd8cac7d9ae6d13f5b41d675590d
Taylor Lei <taylor.lei@tum.de>
2019-03-28 17:50:36 +0100

    improved css with mobile styling/responsive design, fixed css bugs. animations moved to css.

--- app/components/common/ForTheThrone/ForTheThrone.jsx
+++ app/components/common/ForTheThrone/ForTheThrone.jsx
@@ -26,9 +26,9 @@ export default class ForTheThrone extends Component {
 
     componentWillMount (){
         Store.addChangeListener(this._onChange.bind(this));
-
+        
         let chars = this.getRandAntagonistChars();
-        let smallChars = this.getRandSmallChars(chars);
+        let smallChars = this.getRandSmallChars();
         this.setState({
             charLeft: chars[0],
             charRight: chars[1],
@@ -80,14 +80,14 @@ export default class ForTheThrone extends Component {
         return chars;
     }
 
-    getRandSmallChars(chars) {
+    getRandSmallChars() {
         let smallChars = [];
         let smallCharsElems = [];
         let keys = Object.keys(antagonistCharacters.characters);
 
         while (smallChars.length < 5) {
             let rand = Math.floor(Math.random() * keys.length);
-            if (chars[1] !== keys[rand] && chars[0] !== keys[rand] && smallChars.indexOf(keys[rand]) === -1) {
+            if (smallChars.indexOf(keys[rand]) === -1) {
                 smallChars.push(keys[rand]);
                 smallCharsElems.push({
                     name: antagonistCharacters.characters[keys[rand]].name,
@@ -102,100 +102,19 @@ export default class ForTheThrone extends Component {
     focus() {
         $( "#aiLink" ).animate({
             opacity: "1"
-        });
+        }, 0);
 
-        $("#fttHeader, #fttWhoDoYouChoose, .fttTombstoneQuestionMark").animate({
+        $("#fttWhoDoYouChoose").animate({
             opacity: "0"
-        });
-    }
-
-    focusLeftChar() {
-        this.focus();
-
-        $( "#fttCharLeftImg" ).animate({
-            left: "140",
-            bottom: "0",
-            height: "600"
-        });
-
-        $( "#fttCharRightImg" ).animate({
-            bottom: "0",
-            height: "350"
-        });
-
-        $( "#fttThrone" ).animate({
-            marginLeft: "0",
-            left: "100",
-            bottom: "0",
-            height: "600"
-        });
-
-        $( "#fttCharLeftText" ).animate({
-            opacity: "1"
-        });
-    }
-
-    focusRightChar() {
-        this.focus();
-
-        $( "#fttCharRightImg" ).animate({
-            right: "140",
-            bottom: "0",
-            height: "600"
-        });
-
-        $( "#fttCharLeftImg" ).animate({
-            bottom: "0",
-            height: "350"
-        });
-
-        $( "#fttThrone" ).animate({
-            marginLeft: "0",
-            left: "550",
-            bottom: "0",
-            height: "600"
-        });
-
-        $( "#fttCharRightText" ).animate({
-            opacity: "1"
-        });
-    }
-
-    unfocus() {
-        $( "#fttCharLeftImg" ).animate({
-            left: "0",
-            bottom: "0",
-            height: "500"
-        });
-
-        $( "#fttCharRightImg" ).animate({
-            right: "0",
-            bottom: "0",
-            height: "500"
-        });
-
-        $( "#fttThrone" ).animate({
-            right: "0",
-            left: "-200",
-            marginLeft: "50%",
-            bottom: "0",
-            height: "450"
-        });
-
-        $( "#fttCharLeftText, #fttCharRightText, #fttCharLeftTSPLOD, #fttCharRightTSPLOD" ).animate({
-            opacity: "0"
-        });
-
-        $("#fttHeader, .fttTombstoneQuestionMark").animate({
-            opacity: "1"
-        });
+        }, 0);
     }
 
     render() {
         return (
             <div id="fttContainer">
-                <div className="fttCharArea fttLeft" onMouseEnter={this.focusLeftChar.bind(this)} onMouseLeave={this.unfocus.bind(this)}></div>
-                <div className="fttCharArea fttRight" onMouseEnter={this.focusRightChar.bind(this)} onMouseLeave={this.unfocus.bind(this)}></div>
+                <div className="fttCharArea fttLeft" onMouseEnter={this.focus.bind(this)}></div>
+                <div className="fttCharArea fttRight" onMouseEnter={this.focus.bind(this)}></div>
+
                 <div id="fttAntagonists">
                     <img src="ForTheThrone/img/iron-throne.png" id="fttThrone" />
                     <div id="fttHeader" className="center">
@@ -203,6 +122,7 @@ export default class ForTheThrone extends Component {
                         <h3>When you play the game of thrones you win...</h3>
                         <h1 className="center">...or you die</h1>
                     </div>
+
                     <div className="fttCharContainer fttLeft">
                         <img src={this.state.charLeft.img} id="fttCharLeftImg" className="fttCharImg fttLeft" />
                         <div className="fttCharTombstone fttCharLeftTombstone fttTombstonePlod">
@@ -231,17 +151,17 @@ export default class ForTheThrone extends Component {
                     <div id="fttWhoDoYouChoose" className="fttCard">
                         <h4 className="center">Who do you choose</h4>
                         <h2 className="center">#ForTheThrone?</h2>
-                        <p>Move your mouse over a character to discover their<br /><b>Predicted Likelihood of Death</b> in GoT: Season 8!</p>
+                        <p className="center">Click or hover over a character to discover their<br /><b>Predicted Likelihood of Death</b> in GoT: Season 8!</p>
                     </div>
 
                     <div id="aiLink" className="fttCard">
                         <h3 className="center" style={{"marginTop": "0px"}}>A Song of ice and data:</h3>
                         <h4 className="center">Westeros and Machine learning</h4>
                         <p>Our in-house developed machine learning algorithm predicts likelihood of death based on various 
-                        features that we extracted for each of the more than 2000 characters in George&nbsp;R.&nbsp;R.&nbsp;Martin's 
-                        <i> A&nbsp;Song&nbsp;of&nbsp;Ice&nbsp;and&nbsp;Fire</i> series</p>
+                        features that we extracted for each of the more than 2000 characters in George R.&nbsp;R. Martin's 
+                        <i> A&nbsp;Song of&nbsp;Ice and&nbsp;Fire</i> series</p>
                         <div className="center">
-                            <a href="/machine-learning-algorithm-predicts-death-game-of-thrones">
+                            <a id="readMoreLink" href="/machine-learning-algorithm-predicts-death-game-of-thrones">
                                 <b>Read more about our prediction algorithm.</b>
                             </a>
                         </div>
@@ -250,9 +170,7 @@ export default class ForTheThrone extends Component {
                 </div>
                 <div id="fttCountdown">
                     <div className="content">
-                        <div id="fttCountdownLeft">
-                            <br />
-                            <h4 className="center">The next episode airs in</h4>
+                        <div id="fttCountdownLeft"><h4 className="center">The next episode airs in</h4>
                             <Countdown date="April 15, 2019, 03:00:00"></Countdown>
                         </div>
                         <div id="fttCountdownRight">
@@ -262,9 +180,9 @@ export default class ForTheThrone extends Component {
                 </div>
                 <div id="fttCharacters">
                     <div className="content">
-                        {this.state.smallChars.map(function(elem) {
+                        {this.state.smallChars.map(function(elem, index) {
                             return (
-                                <a target="_blank" className="fttCharacter" href={"/characters/" + elem.name}>
+                                <a target="_blank" className="fttCharacter" href={"/characters/" + elem.name} key={index}>
                                     <img src={"/ForTheThrone/img/"+elem.key+"Small.png"} />
                                     <div>{elem.name}</div>
                                 </a>
