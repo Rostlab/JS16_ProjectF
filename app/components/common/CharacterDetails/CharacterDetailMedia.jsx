4a2bdead46f4da4404894c27504b6cb6c722b7f5
Taylor Lei <taylor.lei@tum.de>
2019-03-31 10:09:31 +0200

    fixed bugs in comparison messages

--- app/components/common/CharacterDetails/CharacterDetailMedia.jsx
+++ app/components/common/CharacterDetails/CharacterDetailMedia.jsx
@@ -36,6 +36,14 @@ export default class CharacterDetailsMedia extends Component {
         }
     }
 
+    charPronounPosessive(capitalize = false) {
+        if (capitalize) {
+            return this.character.male ? 'His' : 'Her';
+        } else {
+            return this.character.male ? 'his' : 'her';
+        }
+    }
+
     determineSbPlodDiff() {
         let sbGraphic = (<i className="fas fa-skull"></i>);
 
@@ -54,14 +62,14 @@ export default class CharacterDetailsMedia extends Component {
                     <span> -&nbsp;Beric&nbsp;Dondarrion</span>
                 </p>
             );
-        } else if (this.character.show && this.character.show.death && this.character.book && !this.character.dateOfDeath) {
+        } else if (this.character.show && this.character.show.death && !this.character.show.alive && this.character.book && !this.character.dateOfDeath) {
             sbPlodTitle = this.character.name + " is still going strong in the books.";
-            sbPlodTextFinal  =  <p>{this.character.name} is still alive has a {this.plodBooks}&nbsp;% predicted likelihood of death in the books, 
-                whereas {this.charPronoun()}|s dead in the TV show</p>;
+            sbPlodTextFinal  =  <p>{this.character.name} is still alive has a {this.plodBook}&nbsp;% predicted likelihood of death in the books, 
+                whereas {this.charPronoun()}'s dead in the TV show</p>;
         } else if (this.character.show && this.character.show.alive && this.character.book && this.character.dateOfDeath) {
             sbPlodTitle = this.character.name + " is still going strong in the TV show.";
             sbPlodTextFinal  =  <p>{this.character.name} is still alive has a {this.plodShow}&nbsp;% predicted likelihood of death in the TV show, 
-                whereas {this.charPronoun()}|s dead in the books</p>;
+                whereas {this.charPronoun()}'s dead in the books</p>;
         }  else {
             if (this.plodShow < 25 && this.plodBook < 25) {
                 sbGraphic = <i className="fas fa-award"></i>;
@@ -96,7 +104,7 @@ export default class CharacterDetailsMedia extends Component {
                 sbPlodText  = (<span>that's almost the same.</span>);
             }
 
-            sbPlodTextFinal = <p>{this.character.name} has a {this.plodShow}&nbsp;% predicted likelihood of death in the TV show, whereas her likelihood of death in the books 
+            sbPlodTextFinal = <p>{this.character.name} has a {this.plodShow}&nbsp;% predicted likelihood of death in the TV show, whereas {this.charPronounPosessive()} likelihood of death in the books 
             is {this.plodBook}&nbsp;%, {sbPlodText}</p>;
         }
         this.blocks.push({
@@ -117,6 +125,10 @@ export default class CharacterDetailsMedia extends Component {
             showBirth  = this.character.show.birth;
         }
 
+        if (!booksBirth && !showBirth) {
+            return;
+        }
+
         let sbGraphic;
 
         let sbPlodTitle, sbPlodText, sbPlodQuote = (
@@ -126,38 +138,60 @@ export default class CharacterDetailsMedia extends Component {
             </p>
         );
        
-        var isDead = this.character.show && this.character.show.death || this.character.book && this.character.dateOfDeath;
-        let ifWasntDeadString = isDead ? ', if '+this.charPronoun()+' weren\'t dead' : '';
+        var isDead = this.character.show && this.character.show.death && !this.character.show.alive || this.character.book && this.character.dateOfDeath;
         let isDeadTitleVerb = isDead ? 'was' : 'is';
-        if (!booksBirth && !showBirth) {
-            return;
-        } else if (!booksBirth || !showBirth) {
+
+        let booksAge = booksBirth ? (isDead && this.character.book.dateOfDeath ? this.character.book.dateOfDeath : this.TV_YEAR) - booksBirth : false;
+        let showAge = showBirth ? (isDead && this.character.show.death ? this.character.show.death : this.TV_YEAR) - showBirth : false;
+        let ageDiff = Math.abs(showAge - booksAge);
+        
+        if (!booksBirth || !showBirth) {
             sbPlodTitle = this.character.name + "'s age is mysterious";
             if (booksBirth) {
-                sbPlodText  = (<span><b>{this.TV_YEAR - booksBirth}&nbsp;years&nbsp;old</b> in the books{ifWasntDeadString}. We don't know their age in the show</span>);
+                sbPlodText  = (<span><b>{booksAge}&nbsp;years&nbsp;old</b> in the books. We don't know {this.charPronounPosessive()} age in the show</span>);
             } else {
-                sbPlodText  = (<span><b>{this.TV_YEAR - showBirth}&nbsp;years&nbsp;old</b> in the TV show{ifWasntDeadString}. We don't know their age in the books</span>);
+                sbPlodText  = (<span><b>{showAge}&nbsp;years&nbsp;old</b> in the TV show. We don't know {this.charPronounPosessive()} age in the books</span>);
             }
         } else if (showBirth == booksBirth) {
             sbPlodTitle = this.character.name + " " + isDeadTitleVerb+ " the same age in both the books and the TV show";
-            sbPlodText  = (<span><b>{this.TV_YEAR - booksBirth}&nbsp;years&nbsp;old</b>{ifWasntDeadString}</span>);
+            sbPlodText  = (<span><b>{booksAge}&nbsp;years&nbsp;old</b></span>);
         } else if (showBirth > booksBirth) {
             sbPlodTitle = this.character.name + " " + isDeadTitleVerb+ " younger in the TV show";
-            sbPlodText  = (<span><b>{this.TV_YEAR - booksBirth}&nbsp;years&nbsp;old</b> in the books{ifWasntDeadString}. {this.charPronoun(true)} is <b>{showBirth - booksBirth} year{(showBirth - booksBirth) > 1 ? 's' : ''} younger</b> in the show</span>);
+            sbPlodText  = (<span><b>{booksAge}&nbsp;years&nbsp;old</b> in the 
+                books. {this.charPronoun(true)} is <b>{ageDiff} year{(ageDiff) > 1 ? 's' : ''} younger</b> in the show</span>);
         } else if (showBirth < booksBirth) {
             sbPlodTitle = this.character.name + " " + isDeadTitleVerb+ " older in the TV show";
-            sbPlodText  = (<span><b>{this.TV_YEAR - showBirth}&nbsp;years&nbsp;old</b> in the TV show{ifWasntDeadString}. {this.charPronoun(true)} is <b>{booksBirth - showBirth} year{(booksBirth - showBirth) > 1 ? 's' : ''}</b> younger in the books</span>);
+            sbPlodText  = (<span><b>{showAge}&nbsp;years&nbsp;old</b> in the 
+                TV show. {this.charPronoun(true)} is <b>{ageDiff} year{(ageDiff) > 1 ? 's' : ''}</b> younger in the books</span>);
         }
 
         if (booksBirth || showBirth) {
-            let age = showBirth ? this.TV_YEAR - showBirth : this.TV_YEAR - booksBirth;
+            let age = showAge ? showAge : booksAge;
             sbGraphic  = (<div><i className="fas fa-birthday-cake"></i>
             <h3 className="center" style={{fontSize: '1.1em', marginBottom: '0'}}>{age}</h3>
             <h4 className="center" style={{margin: '0'}}>years</h4></div>);
         }
 
-        let sbPlodTextFinal = (<div><p>If we assume that the current year is {this.TV_YEAR} AC in both the books and the show, 
-            then {this.character.name} {isDead ? 'would be' : 'is'} {sbPlodText}.</p>
+        let dateOfDeath;
+        let aliveInBooks = "";
+        if (isDead) {
+            let dateOfDeathBooks = (<span>{this.character.book.dateOfDeath} AC in the books</span>);
+            let dateOfDeathShow = (<span>{this.character.show.death} AC in the TV show</span>);
+
+            dateOfDeath = (this.character.show.death && this.character.book.dateOfDeath) ? (<span>{dateOfDeathBooks} and {dateOfDeathShow}</span>) 
+                : (this.character.show.death ? dateOfDeathShow : dateOfDeathBooks);
+            if (this.character.show.death == this.character.book.dateOfDeath) {
+                dateOfDeath = (<span>{this.character.show.death} AC in both the books and the TV show</span>);
+            }
+
+            if (!this.character.book.dateOfDeath && booksAge < 50){
+                aliveInBooks = (<span>{this.charPronoun(true)} is still alive in the books.</span>);
+            }
+        }
+
+        let sbPlodTextFinal = (<div><p>
+            {!isDead ? (<span>If we assume that the current year is {this.TV_YEAR} AC in both the books and the show, then </span>) : (<span>{this.character.name} died in {dateOfDeath}. We know that </span>)} 
+            {this.charPronoun()} {isDead ? 'was' : 'is'} {sbPlodText}. {aliveInBooks}</p>
         <p>Ususally characters are <b>older in the TV show</b>, which may contribute to the different predicted values between the two sources, 
             as we use a character's age if it's available.</p></div>);
         
